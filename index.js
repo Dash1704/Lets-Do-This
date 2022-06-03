8ba3ac2a684765d44945d4dc549ead354adbff92
Dashiell Boyles <dashiellboyles@hotmail.com>
2022-06-03 11:29:09 +0100

    html file set and can now see lidt of names when button pressed

--- index.js
+++ index.js
@@ -1,31 +1,35 @@
-const fetch = require("node-fetch");
-
 const getNames = async (eventId) => {
   const raceData = await fetch('https://ldt-tech-test.herokuapp.com/api/startlistentries')
   const raceDataView = await raceData.json()
 
-  const arrayOfNames = raceDataView.map((info) => {
-    if(eventId == info.eventId){
-      console.log(`${info.firstName} ` + `${info.lastName}`)
-    }
-  })
-  console.log(arrayOfNames)
-}
-
-const getIntake = async (organiserId) => {
-  const raceData = await fetch('https://ldt-tech-test.herokuapp.com/api/startlistentries')
-  const raceDataView = await raceData.json()
-
-  let sum = 0
+  const arrayOfNames = []
   raceDataView.map((info) => {
-    if(organiserId == info.organiserId){
-      sum += info.ticketPrice.value
+    if(eventId == info.eventId){
+      arrayOfNames.push(`${info.firstName} ` + `${info.lastName}`)
     }
   })
-  console.log(sum)
+  return arrayOfNames
 }
 
+// const getIntake = async (organiserId) => {
+//   const raceData = await fetch('https://ldt-tech-test.herokuapp.com/api/startlistentries')
+//   const raceDataView = await raceData.json()
 
+//   let sum = 0
+//   raceDataView.map((info) => {
+//     if(organiserId == info.organiserId){
+//       sum += info.ticketPrice.value
+//     }
+//   })
+//   console.log(sum)
+// }
 
-module.exports = getNames
-module.exports = getIntake
+const returnNames = async () => {
+  const out1 = document.getElementById('output1')
+  const userInput = document.getElementById('eventid').value
+  const namesOnScreen = getNames(userInput)
+  out1.innerText = await namesOnScreen
+}
+
+document.getElementById('btn1').addEventListener('click', returnNames)
+
